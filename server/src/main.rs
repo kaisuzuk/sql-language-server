05dcf13536c7ef0df965715428d60fa1e66e3703
kaisuzuk <suzuki826@gmail.com>
2024-01-08 02:02:40 +0900

    Refactor: split into server and client
    テストを書くために、server と client を分離する。

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,17 +3,21 @@ use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
 #[derive(Debug)]
-struct Backend {
+pub struct Backend {
     client: Client,
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        let completion_option = Some(CompletionOptions {
+            trigger_characters: Some(vec![".".to_string(), " ".to_string()]),
+            ..Default::default()
+        });
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
                 hover_provider: Some(HoverProviderCapability::Simple(true)),
-                completion_provider: Some(CompletionOptions::default()),
+                completion_provider: completion_option,
                 ..Default::default()
             },
             ..Default::default()
@@ -30,16 +34,24 @@ impl LanguageServer for Backend {
         Ok(())
     }
 
-    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        self.client
+            .log_message(MessageType::INFO, format!("completion: {:?}", params))
+            .await;
         Ok(Some(CompletionResponse::Array(vec![
             CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
             CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
         ])))
     }
 
-    async fn hover(&self, _: HoverParams) -> Result<Option<Hover>> {
+    async fn hover(&self, params: HoverParams) -> Result<Option<Hover>> {
+        self.client
+            .log_message(MessageType::INFO, format!("hover: {:?}", params))
+            .await;
         Ok(Some(Hover {
-            contents: HoverContents::Scalar(MarkedString::String("You're hovering!".to_string())),
+            contents: HoverContents::Scalar(MarkedString::String(
+                "You're hovering!!!!".to_string(),
+            )),
             range: None,
         }))
     }
